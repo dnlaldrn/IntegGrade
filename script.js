afecee72583deaf6f4c28fda2f9f7edb37354aae
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2025-07-21 10:47:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,39 @@ displayGrade = () =>{
             document.querySelector('.examHPscore').value='';
             document.querySelector('.display').innerHTML = '';
         }
+calculateGrade = ()=>{
+        const quizrawScore = document.querySelector('.quizrawScore').value;
+        const quizHPscore = document.querySelector('.quizHPscore').value;
+        const examRawScore = document.querySelector('.examRawScore').value;
+        const examHPscore = document.querySelector('.examHPscore').value;
+        const quizPercentage = ((quizrawScore / quizHPscore)* 50+ 50)*.40;
+        const examPercentage = ((examRawScore / examHPscore)* 50+ 50)*.60;
+         
+        const totalGrade = Math.round (quizPercentage + examPercentage);
+         
+        if (totalGrade >= 98){
+            return '1.00';
+        }else if(totalGrade >= 94){
+            return "1.25";
+        }else if(totalGrade >= 90){
+            return '1.50';
+        }else if(totalGrade >= 88){
+            return" 1.75";
+        }else if(totalGrade >= 85){
+            return '2.00';
+        }else if(totalGrade >= 83){
+            return "2.25";
+        }else if(totalGrade >= 80){
+            return "2.50";
+        }else if(totalGrade >= 78){
+            return "2.75";
+        }else if(totalGrade >= 75){
+            return '3.00';
+        }else if(totalGrade >= 70){
+            return "INC";
+        }else{
+            return "5.00";
+        }
+        
+        
+    }
