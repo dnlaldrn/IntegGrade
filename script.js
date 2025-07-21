866db96ea8c09bd6bfe7a8fbd62dd862860c1add
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2025-07-21 10:27:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-
+displayGrade = () =>{
+            const convertedGrade = calculateGrade();
+            document.querySelector('.display').innerHTML = `Your converted grade is ${convertedGrade}`
+             if(
+            document.querySelector('.display').innerHTML == `` ||
+            document.querySelector('.quizrawScore').value =='' ||
+            document.querySelector('.quizHPscore').value == '' ||
+            document.querySelector('.examRawScore').value=='' ||
+            document.querySelector('.examHPscore').value==''){
+             document.querySelector('.display').innerHTML = `Not valid input`;   
+            }
+        }
+        deleteDisplay = () =>{
+           
+            if(
+            document.querySelector('.display').innerHTML == `` ||
+            document.querySelector('.quizrawScore').value =='' ||
+            document.querySelector('.quizHPscore').value == '' ||
+            document.querySelector('.examRawScore').value=='' ||
+            document.querySelector('.examHPscore').value==''){
+             document.querySelector('.display').innerHTML = `Not valid input`;   
+            }
+            document.querySelector('.display').innerHTML = ``;
+            document.querySelector('.quizrawScore').value ='';
+            document.querySelector('.quizHPscore').value = '';
+            document.querySelector('.examRawScore').value='';
+            document.querySelector('.examHPscore').value='';
+            document.querySelector('.display').innerHTML = '';
+        }
